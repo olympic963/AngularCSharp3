10ecf17cd44f9833701133e269712ad22a8ed3ac
Nguyễn Anh Tú <olympic963@gmail.com>
2025-01-15 22:51:33 +0700

    Cải tiến phần phân trang

--- test-app/src/app/employee-list/employee-list.component.ts
+++ test-app/src/app/employee-list/employee-list.component.ts
@@ -12,13 +12,17 @@ import { FormsModule } from '@angular/forms';
 })
 export class EmployeeListComponent implements OnInit {
   employees: Employee[] = [];
-  currentPage: number = 1; 
+  currentPage: number = 1;
   itemsPerPage: number = 5;
-  searchName: string = ''; 
-  searchPosition: string = ''; 
+  searchName: string = '';
+  searchPosition: string = '';
   sortBySalary: string = '';
+  startPage: number = 1;
+  endPage: number = 5;
+  ellipsisFirst: boolean = false;
+  ellipsisEnd: boolean = false;
 
-  constructor(private employeeService: EmployeeService, private router: Router, private cdr: ChangeDetectorRef) {}
+  constructor(private employeeService: EmployeeService, private router: Router, private cdr: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.getEmployees();
@@ -28,24 +32,25 @@ export class EmployeeListComponent implements OnInit {
     this.employeeService.searchEmployees(this.searchName, this.searchPosition, this.sortBySalary).subscribe((data) => {
       this.employees = data;
       if (this.currentPage > this.totalPages) {
-        this.currentPage = this.totalPages; 
+        this.currentPage = this.totalPages;
       }
+      this.updateEllipsis();
     });
   }
 
   changeItemsPerPage(itemsPerPage: number): void {
     this.itemsPerPage = +itemsPerPage;
     this.currentPage = 1;
-    this.getEmployees(); 
+    this.getEmployees();
   }
-  
+
   searchEmployees(): void {
-    this.getEmployees(); 
+    this.getEmployees();
   }
 
   sortEmployees(order: string): void {
-    this.sortBySalary = order;  
-    this.getEmployees();  
+    this.sortBySalary = order;
+    this.getEmployees();
   }
 
   get paginatedEmployees(): Employee[] {
@@ -59,11 +64,40 @@ export class EmployeeListComponent implements OnInit {
 
   changePage(page: number): void {
     this.currentPage = page;
+    this.updateEllipsis(); 
+  }
+
+  goToFirstPage(): void {
+    this.currentPage = 1;
+    this.updateEllipsis();
+  }
+
+  goToLastPage(): void {
+    this.currentPage = this.totalPages;
+    this.updateEllipsis(); 
+  }
+
+  updateEllipsis(): void {
+    this.startPage = Math.max(1, this.currentPage - 2);
+    this.endPage = Math.min(this.totalPages, this.startPage + 4);
+    if (this.currentPage >= this.totalPages - 1) {
+      this.startPage = Math.max(1, this.totalPages - 4);
+    }
+    this.ellipsisFirst = this.startPage > 1;
+    this.ellipsisEnd = this.endPage < this.totalPages;
+  }
+
+  get pageRange(): number[] {
+    const range: number[] = [];
+    for (let i = this.startPage; i <= this.endPage; i++) {
+      range.push(i);
+    }
+    return range;
   }
 
   addEmployee(): void {
     this.router.navigate(['/add-employee']).then(() => {
-      this.getEmployees(); 
+      this.getEmployees();
     });
   }
 
@@ -76,14 +110,13 @@ export class EmployeeListComponent implements OnInit {
   deleteEmployee(id: number): void {
     this.employeeService.deleteEmployee(id).subscribe({
       next: (response) => {
-        alert(response.message); 
-        this.getEmployees(); 
-        console.log("Tp: ", this.currentPage);
+        alert(response.message);
+        this.getEmployees();
       },
       error: (error) => {
         alert('Xóa nhân viên thất bại');
         console.error(error);
       }
-    });  
-  }  
+    });
+  }
 }
